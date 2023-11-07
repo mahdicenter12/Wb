c1012eea129af85328dc9dbcc43c700264de6973
mahdicenter12 <147505987+mahdicenter12@users.noreply.github.com>
2023-11-07 05:46:02 -0800

    s06.js

--- s06.js
+++ s06.js
@@ -1,15 +1,50 @@
+
 let http = require('http');
 let server = http.createServer(requestHandler);
-server.listen(8055);
+let port = "8087"
+server.listen(port);
+console.log("Server is running!" + port + "request 200!");
+
+let func = {
+    sum: Sum,
+    Last: Last
+}
+
 
 
 let headers = {'Content-Type': 'Text/Plain'};
+function Sum(res){
+        console.log('Sum')
+        res.writeHead(200, headers);
+        
+        res.write("Sum");
+    }
+function Last(res){
+        console.log('Last')
+        res.writeHead(200, headers);
+        res.write("Last");
+
+    }
+   // "favicon.ico": function(){
+      //  console.log('favicon')
+
+  //  }
+    //}
+    
+
 
 function requestHandler (request, response){
-    console.log('request recived url: ', request.url);
+    console.log('request url: ', request.url);
     console.log('request: ', request.method);
+    let firstpart = request.url.split('/')[1];
 
-    response.writeHead(200, headers);
-    response.write('hi');
+    if (firstpart !== "favicon.ico"){
+        funcs [firstpart(response)]
+    }
+    
+
+
+    
+    
     response.end();
 }
